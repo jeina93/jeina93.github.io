40d29cc1abe906bb9a609605d2bb543096d2ae8a
jeina93 <163663629+jeina93@users.noreply.github.com>
2024-07-03 22:50:44 +0900

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 // Header에 페이지 아래로 스크롤시 다크 스타일링 적용
-
-// window.document.querySelector(); window:cloval 객체
 const header = document.querySelector('.header');
-// console.log(header);
 const headerHeight = header.getBoundingClientRect().height;
 // document.addEventListener('scroll', function () {})
 document.addEventListener('scroll', () => {
-    // 스크롤되는 Y좌표가 headerHeight보다 크다면 다른 스타일링!
     if(window.scrollY > headerHeight) {
         header.classList.add('header--dark');
     } else {
@@ -15,17 +11,10 @@ document.addEventListener('scroll', () => {
 })
 
 // Home 섹셩을 아래로 스크롤시 투명하게 처리함
-
-// const home = document.getElementById("home");
-// const homeHeight = home.getBoundingClientRect().height;
-// document.addEventListener('scroll', () =>{
-//     home.style.opacity = 1-window.scrollY/homeHeight;
-// })
 const home = document.querySelector('.home__container');
 const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', ()=>{
     home.style.opacity = 1-window.scrollY/homeHeight;
-    console.log(home.style.opacity);
 })
 
 //Arrow up버튼을 아래로 스크롤시 투명하게 처리함
@@ -37,4 +26,16 @@ document.addEventListener('scroll', () => {
     }else{
         arrowUp.style.opacity = 0;
     }
+})
+
+// Navbar 토글버튼 클릭 처리
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+})
+
+// Navbar 메뉴 클릭시 메뉴를 자동으로 닫아줌
+navbarMenu.addEventListener('click', () => {
+    navbarMenu.classList.remove('open');
 })
