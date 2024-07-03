25fab42ee1688c4d83c7e31b3322bf671d11dd11
jeina93 <163663629+jeina93@users.noreply.github.com>
2024-07-03 19:44:07 +0900

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -27,3 +27,14 @@ document.addEventListener('scroll', ()=>{
     home.style.opacity = 1-window.scrollY/homeHeight;
     console.log(home.style.opacity);
 })
+
+//Arrow up버튼을 아래로 스크롤시 투명하게 처리함
+const arrowUp = document.querySelector('.arrow-up');
+arrowUp.style.opacity = 0;
+document.addEventListener('scroll', () => {
+    if(window.scrollY>homeHeight/2){
+        arrowUp.style.opacity = 1;
+    }else{
+        arrowUp.style.opacity = 0;
+    }
+})
