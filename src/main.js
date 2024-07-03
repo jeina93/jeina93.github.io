3acd2c7f9459962576526af09d464bdd27748405
jeina93 <163663629+jeina93@users.noreply.github.com>
2024-07-03 14:22:39 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -7,10 +7,23 @@ const headerHeight = header.getBoundingClientRect().height;
 // document.addEventListener('scroll', function () {})
 document.addEventListener('scroll', () => {
     // 스크롤되는 Y좌표가 headerHeight보다 크다면 다른 스타일링!
-    console.log(window.scrollY);
     if(window.scrollY > headerHeight) {
         header.classList.add('header--dark');
     } else {
         header.classList.remove('header--dark');
     }
+})
+
+// Home 섹셩을 아래로 스크롤시 투명하게 처리함
+
+// const home = document.getElementById("home");
+// const homeHeight = home.getBoundingClientRect().height;
+// document.addEventListener('scroll', () =>{
+//     home.style.opacity = 1-window.scrollY/homeHeight;
+// })
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', ()=>{
+    home.style.opacity = 1-window.scrollY/homeHeight;
+    console.log(home.style.opacity);
 })
